f93c1865424ba526560f01fa99df2a07050799e0
takuya.sudo <takuya.sudo@livefor.jp>
2025-04-08 14:24:18 +0900

    kickoff

---
+++ README.md
@@ -0,0 +1,7 @@
+# 作成手順
+
+```
+$ cd fvm_vscode_template
+$ fvm use stable
+$ fvm flutter create . --project-name fvm_vscode_template
+```
---
+++ android/app/src/main/kotlin/com/example/fvm_vscode_template/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fvm_vscode_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
